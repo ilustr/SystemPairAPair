484ce302210d9153dfa00f579949358ce18fa4f5
Maxime <maxime.layat@gmail.com>
2014-02-06 09:10:09 +0100

    positionable and co

--- src/syspairapair/Base.java
+++ src/syspairapair/Base.java
@@ -3,13 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package syspairapair;
 
 /**
  *
  * @author maxime
  */
-public class Base {
+public class Base implements  Positionable{
+
+    private Position position;
+
+    public Base(Position position) {
+        this.position = position;
+    }
+
+    public Base() {
+    }
+
+    @Override
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    @Override
+    public Position getPosition() {
+        return this.position;
+    }
+
     
+    
+
 }
---
+++ src/syspairapair/Positionable.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package syspairapair;
+
+/**
+ *
+ * @author maxime
+ */
+public interface Positionable {
+    
+    public void setPosition(Position position);
+    
+    public Position getPosition();
+
+    
+}
