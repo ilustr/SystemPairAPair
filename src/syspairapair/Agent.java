95b68bb6dfd57afb57315e6eb4606474b6b7c6b3
audur <durazaurel@gmail.com>
2014-02-06 08:17:13 +0100

    first commit

---
+++ src/syspairapair/Agent.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package syspairapair;
+
+/**
+ *
+ * @author ilustr
+ */
+public class Agent implements Runnable{
+
+    public void init()
+    {
+        
+    }
+    public void onReceive()
+    {
+
+    }
+    public void end()
+    {
+        
+    }
+    
+    @Override
+    public void run() {
+        
+    }
+    
+}
